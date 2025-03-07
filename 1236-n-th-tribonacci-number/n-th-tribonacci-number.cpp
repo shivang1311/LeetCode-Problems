03cc3358138ceb9f89612b7cfdb8e85644104b96
Shivang Agrawal <119686924+shivang1311@users.noreply.github.com>
2025-03-08 00:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.93%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -4,14 +4,17 @@ public:
     int tribonacci(int n) {
         if(n<2) return n;
         if(n==2) return 1;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int result;
         for(int i=3; i<=n; i++)
         {
-            dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+            result=a+b+c;
+            a=b;
+            b=c;
+            c=result;
         }
-        return dp[n];
+        return result;
     }
 };
