076631ecb797ba52a353b19de2776c2b1be38505
Shivang Agrawal <119686924+shivang1311@users.noreply.github.com>
2025-03-09 23:07:27 +0530

    Time: 39 ms (100.00%) | Memory: 102.6 MB (76.78%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -1,17 +1,22 @@
 class Solution {
- public:
+public:
     // shivang agrawal
-  int numberOfAlternatingGroups(vector<int>& colors, int k) {
-    const int n = colors.size();
-    int ans = 0;
-    int alternating = 1;
-
-    for (int i = 0; i < n + k - 2; ++i) {
-      alternating =
-          colors[i % n] == colors[(i - 1 + n) % n] ? 1 : alternating + 1;
-      if (alternating >= k)
-        ++ans;
+    static int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        const int n=colors.size(), sz=n+k-1;
+        int ans=0, alt=1, prev=colors[0];
+        for(int i=1; i<sz; i++){
+            alt=(prev==colors[i%n])?1:alt+1;
+            if (alt>=k) ans++;
+            prev=colors[i%n];
+        }
+        return  ans;
     }
-    return ans;
-  }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
