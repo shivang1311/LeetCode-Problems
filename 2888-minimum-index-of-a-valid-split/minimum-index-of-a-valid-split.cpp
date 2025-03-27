81378c6f6be290eb0bb258b9970e0b13398eb35f
Shivang Agrawal <119686924+shivang1311@users.noreply.github.com>
2025-03-27 17:01:24 +0530

    Time: 4 ms (77.46%) | Memory: 89.8 MB (85.66%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,18 +1,35 @@
 class Solution {
- public:
- // shivang agrawal
-  int minimumIndex(vector<int>& nums) {
-    const int n = nums.size();
-    unordered_map<int, int> count1;
-    unordered_map<int, int> count2;
-    for (const int num : nums)
-      ++count2[num];
-    for (int i = 0; i < n; ++i) {
-      const int freq1 = ++count1[nums[i]];
-      const int freq2 = --count2[nums[i]];
-      if (freq1 * 2 > i + 1 && freq2 * 2 > n - 1 - i)
-        return i;
+    // shivang agrawal
+    int mooresVotingAlgo(vector<int>& nums){
+        int majority_element = nums[0];
+        int freq = 1;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]!=majority_element)   freq--;
+            else                            freq++;
+            if(freq==0){
+                majority_element = nums[i];
+                freq = 1;
+            }
+        }
+        return majority_element;
     }
-    return -1;
-  }
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n=nums.size();
+        int majority_element = mooresVotingAlgo(nums);
+        int max_freq = 0;
+        for(int i=0;i<n;++i)
+            if(nums[i]==majority_element)
+                max_freq++;
+        int prefix_count = 0;
+        for(int i=0;i<n-1;++i){
+            if(nums[i]==majority_element){
+                prefix_count++;
+                max_freq--;
+            }
+            if((prefix_count > (i+1)/2) and (max_freq > (n-i-1)/2))
+                return i;
+        }
+        return -1;
+    }
 };
