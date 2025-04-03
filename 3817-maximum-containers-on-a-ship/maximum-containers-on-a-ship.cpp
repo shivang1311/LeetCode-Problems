da3ef37ae863ccd2a3e7ba82af9b51edcf4333de
Shivang Agrawal <119686924+shivang1311@users.noreply.github.com>
2025-04-03 10:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.70%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //
+    // shivang agrawal
     int maxContainers(int n, int w, int maxWeight) {
         int maxcells = n*n;
         int max_container = maxWeight/w;
