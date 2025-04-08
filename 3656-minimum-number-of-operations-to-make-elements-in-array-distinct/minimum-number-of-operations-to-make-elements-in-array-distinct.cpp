2215072342aad87fd1804a4771bedff1763add3d
Shivang Agrawal <119686924+shivang1311@users.noreply.github.com>
2025-04-08 12:44:34 +0530

    Time: 2 ms (61.43%) | Memory: 27.8 MB (43.34%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+ // shivang agrawal
+  int minimumOperations(vector<int>& nums) {
+    unordered_set<int> seen;
+    for (int i = nums.size() - 1; i >= 0; --i)
+      if (!seen.insert(nums[i]).second)
+        return (i + 1 + 2) / 3; 
+    return 0;
+  }
+};
